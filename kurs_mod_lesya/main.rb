928fdc7786652c0536455fa3da06f2cd3b0a55ec
ikroni <kron@ikroni.info>
2013-11-21 18:05:22 +0400

    Исправлена ошибка в коде

--- kurs_mod_lesya/main.rb
+++ kurs_mod_lesya/main.rb
@@ -22,10 +22,8 @@ while t.now? < t.end?
       qa.enq(1)
       random = '%.2f' % rand
       t.time_ticket_a(t.now?.to_f + 0.007 + 0.006 * random.to_f)
-      puts ca.start_transact
-      if ca.start_transact
-        t.time_trans_a_to_b(t.now?.to_f + 0.01)
-      end
+      #puts ca.start_transact
+      t.time_trans_a_to_b(t.now?.to_f + 0.01) if ca.start_transact        
     else
       if cs.start_transact
         random = '%.2f' % rand
@@ -42,9 +40,7 @@ while t.now? < t.end?
       qb.enq(1)
       random = '%.2f' % rand
       t.time_ticket_b(t.now?.to_f + 0.007 + 0.006 * random.to_f)
-      if cb.start_transact
-        t.time_trans_b_to_a(t.now?.to_f + 0.01)
-      end
+      t.time_trans_b_to_a(t.now?.to_f + 0.01) if cb.start_transact
     else
       if cs.start_transact
         random = '%.2f' % rand
@@ -58,9 +54,10 @@ while t.now? < t.end?
   if not qa.empty?
     if t.now?.to_f > t.time_trans_a_to_b?
       qa.deq
+      ca.end_transact
       chan_a+= 1
       if not qa.empty?
-        t.time_trans_a_to_b(t.now?.to_f + 0.01)
+        t.time_trans_a_to_b(t.now?.to_f + 0.01) if ca.start_transact
       else
         t.time_trans_a_to_b(0)
       end
@@ -70,9 +67,10 @@ while t.now? < t.end?
   if not qb.empty?
     if t.now?.to_f > t.time_trans_b_to_a?
       qb.deq
+      cb.end_transact
       chan_b+=1
       if not qa.empty?
-        t.time_trans_b_to_a(t.now?.to_f + 0.01)
+        t.time_trans_b_to_a(t.now?.to_f + 0.01) if cb.start_transact
       else
         t.time_trans_b_to_a(0)
       end
@@ -81,10 +79,10 @@ while t.now? < t.end?
 
   if t.time_trans_sputnik? != 0
     if t.now?.to_f > t.time_trans_sputnik?
+      cs.end_transact
       chan_sp+=1
       t.time_trans_sputnik(0)
     end
   end
   puts "time #{t.now?.to_f} length A #{qa.length} length B #{qb.length} drop #{droped} tic_a #{tickets_a} tic_b #{tickets_b} chan_a #{chan_a} chan_b #{chan_b} chan_sp #{chan_sp}"
-  puts " "
 end
